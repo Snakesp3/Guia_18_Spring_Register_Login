0065a826fa50cd207dedf43cada273f8cc947b48
Sebastian G <resgirolminipcparts2014@gmail.com>
2023-02-13 21:04:51 -0300

    Actualizando ejercicio propio Spring2 seguridad

--- Ejercicios Fundametales/alumnos2023/src/main/java/com/alumnos/alumnos2023/SeguridadWeb.java
+++ Ejercicios Fundametales/alumnos2023/src/main/java/com/alumnos/alumnos2023/SeguridadWeb.java
@@ -1,7 +1,21 @@
 
 package com.alumnos.alumnos2023;
 
+import com.sun.xml.internal.ws.developer.HttpConfigFeature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SeguridadWeb {
+//Con esto creamos la base de nuestra clase de seguridad
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadWeb extends WebSecurityConfigurerAdapter{
+    
+    protected void configure(HttpConfigFeature http) throws Exception {
+        
+        //Seguir viendo video configuracion inicial en el min 7.05
+    }
     
 }
